151ff6308e611177e892411aa0fcdbbde67013ac
Dustye-0827 <105948515+Dustye-0827@users.noreply.github.com>
2022-05-20 23:42:17 +0100

    ADDED CUSTOM RECIPE GROUPS FOR GOLD AND DEMONITE

--- Kasta/Kasta.cs
+++ Kasta/Kasta.cs
@@ -1,8 +1,27 @@
 using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
 
 namespace Kasta
 {
 	public class Kasta : Mod
 	{
+		public override void AddRecipeGroups()
+		{
+			RecipeGroup group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Evil Bar", new int[]
+			{
+		ItemID.DemoniteBar,
+		ItemID.CrimtaneBar
+			});
+			RecipeGroup.RegisterGroup("Evil Bar", group);
+
+			RecipeGroup group2 = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Gold Bar", new int[]
+			{
+		ItemID.GoldBar,
+		ItemID.PlatinumBar
+	        });
+			RecipeGroup.RegisterGroup("Gold Bar", group2);
+		}
 	}
 }
