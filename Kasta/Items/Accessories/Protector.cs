eb941898f7fc7eca6c123984271866eb0a2f51b7
TNTUPpopcron <105949017+TNTUPpopcron@users.noreply.github.com>
2022-05-21 07:28:24 +0700

    Add files via upload
    Rebalance it, sometime.
    Also change the tooltip

---
+++ Kasta/Items/Accessories/Protector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Kasta.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Shield)]
+    public class Protector : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Kasta's Protector");
+            Tooltip.SetDefault("'something creative here lator.'");
+        }
+        public override void SetDefaults()
+        {
+
+            item.width = 28;
+            item.height = 24;
+            item.accessory = true; // Makes this item an accessory.
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(gold: 1); // Sets the item sell price to one gold coin.
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statDefense += 5;
+            player.lifeRegen += 2;
+            player.allDamageMult *= 1.05f;
+            player.fireWalk = true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.BandofRegeneration);
+            recipe.AddIngredient(mod, "KastaBar", 12);
+            recipe.AddIngredient(ItemID.ObsidianShield);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
