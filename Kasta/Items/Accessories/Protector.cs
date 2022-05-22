29c734e5c6efb4ce57023cbae75fb225eaefd92a
Dustye-0827 <105948515+Dustye-0827@users.noreply.github.com>
2022-05-22 11:16:45 +0100

    updated protector

--- Kasta/Items/Accessories/Protector.cs
+++ Kasta/Items/Accessories/Protector.cs
@@ -13,8 +13,8 @@ namespace Kasta.Items.Accessories
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Kasta's Protector");
-            Tooltip.SetDefault("Defense 6 \nGrants immunity to knockback \nGrants immunity to fireblocks \nIncrease life regen \n5% increase damage");
+            DisplayName.SetDefault("Kasta Shield");
+            Tooltip.SetDefault("Grants immunity to knockback \nGrants immunity to fireblocks \nIncrease life regen \n5% increase damage");
         }
         public override void SetDefaults()
         {
@@ -24,10 +24,10 @@ namespace Kasta.Items.Accessories
             item.accessory = true; // Makes this item an accessory. change this comment sometimes.
             item.rare = ItemRarityID.Blue;
             item.value = Item.sellPrice(gold: 1); // Sets the item sell price to one gold coin.
+            item.defense = 5;
         }
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.statDefense += 5;
             player.lifeRegen += 2;
             player.allDamageMult *= 1.05f;
             player.fireWalk = true;
