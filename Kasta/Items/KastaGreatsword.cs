865d43606ea384c91364e8106d4f6b8041b48f70
Kodex <91547201+planetarymonki@users.noreply.github.com>
2022-05-22 12:30:57 -0300

    Add files via upload

---
+++ Kasta/Items/KastaGreatsword.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Kasta.Items
+{
+    public class KastaGreatsword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Kasta Greatsword");
+            Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 36;
+            item.melee = true;
+            item.width = 64;
+            item.height = 64;
+            item.useTime = 22;
+            item.useAnimation = 22;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.knockBack = 7.5f;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(gold: 1);
+            item.scale = 1.25f;
+            item.UseSound = SoundID.Item1;
+            item.autoReuse = true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddRecipeGroup("Wood", 10);
+            recipe.AddIngredient(ItemID.WormTooth, 2);
+            recipe.AddIngredient(mod, "KastaBar", 20);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Kasta/Items/KastaHamaxe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Kasta.Items
+{
+    public class KastaHamaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Kasta Hamaxe");
+            Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.UseSound = SoundID.Item1;
+            item.melee = true;
+            item.damage = 22;
+            item.width = 44;
+            item.height = 46;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.useAnimation = 27;
+            item.useTime = 27;
+            item.autoReuse = true;
+            item.knockBack = 7;
+            item.hammer = 60;
+            item.axe = 20;
+            item.scale = 1.25f;
+            item.rare = ItemRarityID.Blue;
+            item.value = Item.sellPrice(gold: 1);
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddRecipeGroup("Wood", 10);
+            recipe.AddIngredient(mod, "KastaBar", 20);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
